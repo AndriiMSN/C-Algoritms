cd93a50acdb2032f2ffed5f0217506675f6c96a1
Andrii-M <andrewmosin8@gmail.com>
2021-05-19 02:06:33 +0300

    uorshella

---
+++ Уоршелла.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#define MaxNodes 5
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int m[MaxNodes][MaxNodes] = {
+		{
+			0,
+			1,
+			0,
+			0,
+			0,
+		},
+		{
+			0,
+			0,
+			1,
+			0,
+			0,
+		},
+		{
+			1,
+			0,
+			0,
+			1,
+			0,
+		},
+		{
+			0,
+			0,
+			0,
+			0,
+			0,
+		},
+		{
+			1,
+			0,
+			1,
+			0,
+			0,
+		},
+	};
+	int s[MaxNodes][MaxNodes];
+
+	for (int i = 0; i < MaxNodes; i++)
+	{
+		for (int j = 0; j < MaxNodes; j++)
+		{
+			s[i][j] = m[i][j];
+		}
+	}
+
+	for (int k = 0; k < MaxNodes; k++)
+	{
+		for (int i = 0; i < MaxNodes; i++)
+		{
+			if (s[i][k] == 1)
+			{
+				for (int j = 0; j < MaxNodes; j++)
+				{
+					s[i][j] = (s[i][j] || s[k][j]);
+				}
+			}
+		}
+		cout << "Матрица достижимости:" << k << endl;
+		for (int i = 0; i < MaxNodes; i++)
+		{
+			for (int j = 0; j < MaxNodes; j++)
+			{
+				cout << s[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
